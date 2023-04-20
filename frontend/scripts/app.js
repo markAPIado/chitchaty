6e55a1870feede8731a32ef12f3896c18c3286b1
Mark Apiado <apiadomptc@gmail.com>
2023-04-20 13:01:47 +0800

    Add global loader

--- frontend/scripts/app.js
+++ frontend/scripts/app.js
@@ -1,5 +1,13 @@
 import Router from './Router.js'
 
+// Add loding animation while the page is loading
+
+window.addEventListener('load', () => {
+  document.querySelector('.global-loader').style.display = 'none'
+})
+
+
+
 window.addEventListener('DOMContentLoaded', () => {
   Router.init()
 })
