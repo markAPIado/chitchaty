f53a2318635d001b5557b9e80bc9c0180a39379a
Mark Apiado <apiadomptc@gmail.com>
2023-04-20 16:49:55 +0800

    Clean up uneccesary code

--- backend/middlewares/input-validation.js
+++ backend/middlewares/input-validation.js
@@ -1,19 +1,18 @@
 import Joi from 'joi'
 
-export const validateInput =
-  (input, schema, abortEarly) => (req, res, next) => {
-    const object = Joi.object(schema)
-    const options = { abortEarly }
-    const { error } = object.validate(req.body, options)
-    if (!error) return next()
+export const validateInput = (schema) => (req, res, next) => {
+  const object = Joi.object(schema)
+  const options = { abortEarly: true }
+  const { error } = object.validate(req.body, options)
+  if (!error) return next()
 
-    const errors = {}
+  const errors = {}
 
-    error.details.forEach((item) => {
-      errors.type = 'input'
-      errors.path = item.path[0]
-      errors.message = item.message
-    })
+  error.details.forEach((item) => {
+    errors.type = 'input'
+    errors.path = item.path[0]
+    errors.message = item.message
+  })
 
-    res.status(400).json({ error: errors })
-  }
+  res.status(400).json({ error: errors })
+}
